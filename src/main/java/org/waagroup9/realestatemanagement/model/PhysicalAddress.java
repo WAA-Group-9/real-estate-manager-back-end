9dc6c56038ffcc7ee40c00d43a7c8ac3a1b6dbe6
abenezerwg <aben.wg@gmail.com>
2024-02-06 00:05:04 -0600

    Updated DB

--- src/main/java/org/waagroup9/realestatemanagement/model/PhysicalAddress.java
+++ src/main/java/org/waagroup9/realestatemanagement/model/PhysicalAddress.java
@@ -1,7 +1,16 @@
 package org.waagroup9.realestatemanagement.model;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class PhysicalAddress {
+    private String street;    private String city;
+    private String zipCode;
+    private String country;
 }
