06c3cc3a977539c880a7654505742dfe6d24f9dc
salah <salahdinjemal@gmail.com>
2024-02-06 23:14:41 -0600

    add my list model

---
+++ src/main/java/org/waagroup9/realestatemanagement/model/entity/MyList.java
@@ -0,0 +1,27 @@
+package org.waagroup9.realestatemanagement.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany
+    private List<Property> properties;
+
+}
